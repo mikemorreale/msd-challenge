bcf15b860c8571f7fb8ef0b41d3c684f13265537
Riddhi Mehta <Riddhi_Mehta@mckinsey.com>
2012-07-03 13:03:48 -0400

    minor code cleanup

--- src/solution.py
+++ src/solution.py
@@ -54,20 +54,6 @@ def main():
        
     # Create user colisten dictionary using threads
     print("thread function creating user colisten dictionary")
-#    p1 = Process(target=create_user_colisten, args= (1, 27500, "../results/user_colisten1.txt", play_count))
-#    p2 = Process(target=create_user_colisten, args= (27501, 55000, "../results/user_colisten2.txt", play_count))
-#    p3 = Process(target=create_user_colisten, args= (55001, 82500, "../results/user_colisten3.txt", play_count))
-#    p4 = Process(target=create_user_colisten, args= (82501, 110001, "../results/user_colisten4.txt", play_count))
-#    
-#    p1.start()
-#    p2.start()
-#    p3.start()
-#    p4.start()
-#    
-#    p1.join()
-#    p2.join()
-#    p3.join()
-#    p4.join()
     
     # Populating the user colisten dictionary from the files created by the threads
     print("population user colisten")
@@ -77,8 +63,7 @@ def main():
             common_songs = []
             common_songs = line.strip().split(" ")
             user_colisten[common_songs[0]] = common_songs[1:]
-        
-    
+            
     # create colisten dictionary
     print("creating songs colisten dictionary")
     song_colisten = {}
@@ -91,8 +76,6 @@ def main():
                     song_colisten[song1] = {song2 : 1}
                 elif song2 not in song_colisten[song1]:
                     song_colisten[song1][song2] = 1
-    
-    
     
     # store the diagonal elements of the colisten matrix into an array and sort it
     print("creating array storing colisten diagonal and sorted colisten diagonal")
@@ -151,48 +134,18 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    #user_colisten1 = UserColisten(1, 27500, play_count, "../results/user_colisten1.txt")
-    #user_colisten2 = UserColisten(27501, 55000, play_count, "../results/user_colisten2.txt")
-    #user_colisten3 = UserColisten(55001, 82500, play_count,  "../results/user_colisten3.txt")
-    #user_colisten4 = UserColisten(82501, 110001, play_count, "../results/user_colisten4.txt")
-    #
-    #threads = []
-    #user_colisten1.start()
-    #user_colisten2.start()
-    #user_colisten3.start()
-    #user_colisten4.start()
-    #
-    #threads.append(user_colisten1)
-    #threads.append(user_colisten2)
-    #threads.append(user_colisten3)
-    #threads.append(user_colisten4)
-    #
-    #for thread in threads:
-    #    thread.join()
     
-    #class UserColisten(Thread):    
-    #    def __init__(self, range_start, range_end, play_count, outfile):
-    #        Thread.__init__(self)
-    #        self.range_start = range_start
-    #        self.range_end = range_end
-    #        self.play_count = play_count
-    #        self.outfile = outfile
-    #    
-    #    def run(self):
-    #        user_colisten = {}
-    #        print(str(self.outfile))
-    #        with open(self.outfile, 'w')as file:
-    #            for user1 in range(self.range_start, self.range_end):
-    #                print(user1)       
-    #                max_song_match = -1
-    #                for user2 in range(user1 + 1, self.range_end):
-    #                    temp_colisten = set(self.play_count[user1].keys()) & set(self.play_count[user2].keys())
-    #                    if temp_colisten:
-    #                        song_list_length = len(temp_colisten)
-    #                        if song_list_length > max_song_match:
-    #                            user_colisten[user1] = temp_colisten
-    #                            max_song_match = song_list_length
-    #                file.write(str(user1) +" "+ str(user_colisten[user1]) + "\n")
-    
-    
+#    p1 = Process(target=create_user_colisten, args= (1, 27500, "../results/user_colisten1.txt", play_count))
+#    p2 = Process(target=create_user_colisten, args= (27501, 55000, "../results/user_colisten2.txt", play_count))
+#    p3 = Process(target=create_user_colisten, args= (55001, 82500, "../results/user_colisten3.txt", play_count))
+#    p4 = Process(target=create_user_colisten, args= (82501, 110001, "../results/user_colisten4.txt", play_count))
+#    
+#    p1.start()
+#    p2.start()
+#    p3.start()
+#    p4.start()
+#    
+#    p1.join()
+#    p2.join()
+#    p3.join()
+#    p4.join()
